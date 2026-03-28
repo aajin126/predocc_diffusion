e7e00691967d306debd29d7705f7dfafd5220085
hj <leehahj77@gamil.com>
2026-03-28 11:44:48 +0900

    latent

--- scripts/estimate_latent_stats.py
+++ scripts/estimate_latent_stats.py
@@ -1,19 +1,44 @@
+
 import torch
 from omegaconf import OmegaConf
 from torch.utils.data import DataLoader
+import sys
+import os
+# Add project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from util import instantiate_from_config
+# Patch sys.modules so 'modules' points to 'predocc.modules' and 'util' to 'predocc.util'
+import importlib
+
+try:
+    predocc_modules = importlib.import_module('predocc.modules')
+    sys.modules['modules'] = predocc_modules
+    predocc_util = importlib.import_module('predocc.util')
+    sys.modules['util'] = predocc_util
+    predocc_models = importlib.import_module('predocc.models')
+    sys.modules['models'] = predocc_models
+    predocc_data = importlib.import_module('predocc.data')
+    sys.modules['data'] = predocc_data
+except ImportError:
+    pass
+
+from predocc.util import instantiate_from_config
+
 
 # config load
+
 config = OmegaConf.load("../configs/autoencoder/autoencoder_predocc.yaml")
+# Patch model target to use fully qualified module path if needed
+if "target" in config.model and config.model["target"].startswith("models."):
+    config.model["target"] = "predocc." + config.model["target"] 
 
 # data root fix
-config.data.params.train.params.data_root = "/home/ewhaglab/develop/data/OGM-datasets/OGM-Turtlebot2/train/"
-config.data.params.validation.params.data_root = "/home/ewhaglab/develop/data/OGM-datasets/OGM-Turtlebot2/val/"
+config.data.params.train.params.data_root = "/home/oem/hj/data/OGM-datasets/OGM-Turtlebot2/train/"
+config.data.params.validation.params.data_root = "/home/oem/hj/data/OGM-datasets/OGM-Turtlebot2/val/"
 
 model = instantiate_from_config(config.model)
 ckpt = torch.load(
-    "/home/ewhaglab/develop/predocc_diffusion/models/first_stage_models/predocc_ae/last.ckpt",
+    "/home/oem/hj/predocc_diffusion_v0/pretrained_models/first_stage_models/AE/v2.1/model.ckpt",
     map_location="cpu",
 )
 state_dict = ckpt["state_dict"] if "state_dict" in ckpt else ckpt
@@ -32,4 +57,4 @@ dataloader = DataLoader(
     shuffle=False,
 )
 
-model.estimate_latent_stats(dataloader, num_batches=128, use_mode=False)
+model.estimate_latent_stats(dataloader, num_batches=500, use_mode=False)
