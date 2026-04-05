76826465dc195010ed61c291330836720ecb5393
aajin126 <leehahj77@gmail.com>
2026-04-05 16:03:22 +0900

    eval

--- eval.py
+++ eval.py
@@ -40,7 +40,7 @@ MAP_Y_LIMIT = [-3.2, 3.2]   # Map limits on the y-axis
 RESOLUTION = 0.1        # Grid resolution in [m]'
 TRESHOLD_P_OCC = 0.8    # Occupancy threshold
 all_rows = []    
-csv_path = os.path.join("output", "v7.7.0", "eval_table.csv")
+csv_path = os.path.join("output", "ldm2.2", "eval_table.csv")
 
 def compute_iou(pred, gt, occ_thr=0.3):
     pred_occ = (pred > occ_thr)
@@ -123,15 +123,19 @@ def evaluate_ldm(model, dataloader, device, output_dir, ddim_steps=20, ddim_eta=
                 break
             
             # Get model inputs z, c, x_gt, xrec, xc
-            x_in, x_gt, x_occ_map, x_rel, y_rel, th_rel = model.get_input(
+            x_in, x_gt, _, x_rel, y_rel, th_rel = model.get_input(
                                                             batch,
                                                             process = 'test'
                                                         )
 
             t0 = time.perf_counter()
             c, _ = model.get_encoding(x_in, x_gt)
-            c_exp = c.repeat_interleave(model.first_stage_model.seq_len, dim=0) # (B*T, 32, 16, 16)
 
+            # 1) duplicate condition
+            c = {
+                "c_spade": c["c_spade"].repeat_interleave(num_samples, dim=0),
+            }
+            
             # Measure sampling time
             if device.type == "cuda":
                 torch.cuda.synchronize()
@@ -139,15 +143,16 @@ def evaluate_ldm(model, dataloader, device, output_dir, ddim_steps=20, ddim_eta=
             
             # DDIM sampling from random noise
             with model.ema_scope("Evaluation"):
-                z_samples, _ = model.sample_log(
-                    cond=c_exp,
-                    batch_size=num_samples * SEQ_LEN,
+                z_samples_stacked, _ = model.sample_log(
+                    cond=c,
+                    batch_size=c["c_spade"].shape[0],
                     ddim=True,
                     ddim_steps=ddim_steps,
                     eta=ddim_eta
                 )
             
             # Decode latent to sequence
+            z_samples = model._unstack_time_channel_latent(z_samples_stacked) # (B_vis*K*T, embed_dim, 16, 16)
             pred_seq = model.decode_first_stage(z_samples)  # (num_samples, T, 1, H, W)
 
             # Reprojection for each time step
